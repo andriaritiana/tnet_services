df66f03b557b6ea9e074706dd23168cbe673cea8
iarinaval <iandrinaval@gmail.com>
2018-07-10 21:14:56 +0300

    structure

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ let bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', function (req, res) {
-  console.log("Bonjour api");
-});
+app.use(express.static(__dirname + "/public"));
+app.use(require("./middlewares"));
+app.use(require("./controllers"));
 
 app.listen(8060);
 
